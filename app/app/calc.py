c4cbf2823ec17056daf6c8ea9a7f002a634b890a
Zhi Zheng <15041741+zhizheng7@users.noreply.github.com>
2023-02-09 00:14:00 +1100

    added postgresql engine

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y return results."""
+    return x + y
+
+def subtract(x, y):
+    """subtract x from y and return result"""
+    return y - x
