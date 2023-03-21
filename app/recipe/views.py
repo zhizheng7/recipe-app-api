c6d0c5b7752ef85bc6384af7e3b3f0d479aa551f
Zhi Zheng <15041741+zhizheng7@users.noreply.github.com>
2023-03-22 06:40:57 +1100

    s108a - initial ingredient api list

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -12,6 +12,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
 from recipe import serializers
 
@@ -53,4 +54,16 @@ class TagViewSet(
 
     def get_queryset(self):
         """Filter queryset to authetnicated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in the database."""
+    serializers_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
